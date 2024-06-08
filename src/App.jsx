b38c0a3883f3cde4eebf27ef8db86ae681953592
alexismreyes <alexis_mreyes@hotmail.com>
2024-06-08 16:59:02 -0600

    Se creo lista de criptos desde API - Se utilizo Axios y se creo archivo CSS (falta desarrollarlo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,76 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState()
+  
+  //basic fetch way
+  /* useEffect(()=>{
+    //fetch("https://api.coincap.io/v2/assets")
+    fetch(`${API_URL}assets`)       
+    .then((resp) => resp.json())
+    .then((data)=>{
+      //console.log(data);
+      setCriptos(data.data)
+    })
+    .catch(()=>{
+      console.error("La petición falló");
+    })
+  },[]) */
+
+//Async/Await using fetch
+/*   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        //const response = await fetch("https://api.coincap.io/v2/assets");
+        const url = `${import.meta.env.VITE_API_URL}assets`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setCriptos(data.data);
+      } catch (error) {
+        console.error("La petición falló", error);
+      }
+    };
+
+    fetchData();
+  }, []); */
+
+
+  //axios way
+
+  useEffect(()=>{    
+    axios.get(`${API_URL}assets`)
+    .then((data)=>{
+      //console.log(data);
+      setCriptos(data.data.data)
+    })
+    .catch(()=>{
+      console.error("La petición falló");
+    })
+  },[])
+
+
+  
+
+  if (!criptos) return <span>Cargando...</span>
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <h1>LISTA DE CRIPTOMONEDAS</h1>
+      <ul>
+        {/* { criptos.map( cripto => (
+          <li>Nombre: { cripto.name } - Precio: { cripto.priceUsd }</li>
+        ) ) } */}
+
+        { criptos.map( ({ id, name, priceUsd }) => (
+          <li key={id}> Nombre: { name } - Precio: { priceUsd }</li>
+        ) ) }
+
+      </ul>
+      
     </>
   )
 }
